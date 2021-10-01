580d8f0dce9a2cfdc03d4ef5a71ca1299c78f290
anvaari <anvaari@pm.me>
2021-10-01 18:38:20 +0330

    Add Comment
    Add comment, so you can understand code better

--- FireFox History/sqlite2csv.py
+++ FireFox History/sqlite2csv.py
@@ -11,14 +11,14 @@ script_path=os.path.dirname(os.path.abspath(__file__))
 
 def places2csv(db_path,output_path=script_path):
     '''
+    places2csv get database path as input and extract places info from it, return it as dataframe and save it in output path as CSV.
     
-
     Parameters
     ----------
     db_path : str
-        Path to places.sqlite.
+        Path to places.sqlite
     output_path : str
-        Path where csv files will save.
+        Path to where you want to save CSV
 
     Returns
     -------
@@ -27,7 +27,7 @@ def places2csv(db_path,output_path=script_path):
         Also save places DataFrame in output folder with csv format.
 
     '''
-    con=sqlite3.connect(db_path)
+    con=sqlite3.connect(db_path) # Connect to database
     places=pd.read_sql_query('SELECT * FROM moz_places',con)
     if __name__=='__main__':
         places.to_csv(output_path+'/places.csv')
@@ -36,14 +36,15 @@ def places2csv(db_path,output_path=script_path):
     
 def history_v2csv(db_path,output_path=script_path):
     '''
-    
+    history_v2csv get database path as input and extract history info from it, return it as dataframe and save it in output path as CSV.
+
 
     Parameters
     ----------
     db_path : str
-        Path to places.sqlite.
+        Path to places.sqlite
     output_path : str
-        Path where csv files will save.
+        Path to where you want to save CSV
 
     Returns
     -------
@@ -53,7 +54,7 @@ def history_v2csv(db_path,output_path=script_path):
 
 
     '''
-    con=sqlite3.connect(db_path)
+    con=sqlite3.connect(db_path) # Connect to database
     history_v=pd.read_sql_query('SELECT * FROM moz_historyvisits', con)
     if __name__=='__main__':
         history_v.to_csv(output_path+'/history_visit.csv')
@@ -62,14 +63,15 @@ def history_v2csv(db_path,output_path=script_path):
 
 def origins2csv(db_path,output_path=script_path):
     '''
-    
+    origins2csv get database path as input and extract origins info from it, return it as dataframe and save it in output path as CSV.
+
 
     Parameters
     ----------
     db_path : str
-        Path to places.sqlite.
+        Path to places.sqlite
     output_path : str
-        Path where csv files will save.
+        Path to where you want to save CSV
 
     Returns
     -------
@@ -77,7 +79,7 @@ def origins2csv(db_path,output_path=script_path):
         moz_originss table from places.sqlite in DataFrame fromat.
         Also save origins DataFrame in output folder with csv format.
     '''
-    con=sqlite3.connect(db_path)
+    con=sqlite3.connect(db_path)  # Connect to database
     origins=pd.read_sql_query('SELECT * FROM moz_origins', con)
     if __name__=='__main__':
         origins.to_csv(output_path+'/origins.csv')
@@ -88,6 +90,7 @@ def origins2csv(db_path,output_path=script_path):
     
     
 if __name__=='__main__':
+    # Parse arguments
     parser=argparse.ArgumentParser(description='Convert your firefox history from sqlite to csv format')
     parser.add_argument(
         '--db-path',
@@ -101,11 +104,14 @@ if __name__=='__main__':
         type=str,
         default=script_path,
         help='Path to where you want csv files save')
+    # Assign argumnets to variables 
     args=parser.parse_args()
     db_path=args.db_path
     output_path=args.output_path
-    p=places2csv(db_path,output_path)
-    v=history_v2csv(db_path, output_path)
-    o=origins2csv(db_path, output_path)
+    
+    # Export tables in places.sqlite as CSV
+    places2csv(db_path,output_path)
+    history_v2csv(db_path, output_path)
+    origins2csv(db_path, output_path)
     
     
